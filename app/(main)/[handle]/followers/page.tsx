b002e6d476df36380d0e7122ba674080f32d399d
Siwoo Jeon <me@migan.co.kr>
2025-12-17 08:43:09 +0900

    fix: add try/catch

--- app/(main)/[handle]/followers/page.tsx
+++ app/(main)/[handle]/followers/page.tsx
@@ -1,18 +1,33 @@
 import { UserList } from "@/components/users/UserList";
 import { auth } from "@/lib/auth";
-import { getUsersWithFollowers } from "@/lib/services/user";
+import {
+	getUsersWithFollowers,
+	type UsersWithFollowing,
+} from "@/lib/services/user";
 
 export default async function FollowersPage({
 	params,
 }: {
 	params: Promise<{ handle: string }>;
 }) {
+	let users: UsersWithFollowing;
+
 	const { handle } = await params;
 	const session = await auth();
-	const users = await getUsersWithFollowers(
-		handle,
-		session ? Number(session.user?.id) : 0,
-	);
+
+	try {
+		users = await getUsersWithFollowers(
+			handle,
+			session ? Number(session.user?.id) : 0,
+		);
+	} catch (err) {
+		console.log(err);
+		return (
+			<div>
+				<h1>유저를 로드 중 오류 발생.</h1>
+			</div>
+		);
+	}
 
 	if (!users.length)
 		return (
--- app/(main)/[handle]/following/page.tsx
+++ app/(main)/[handle]/following/page.tsx
@@ -1,18 +1,33 @@
 import { UserList } from "@/components/users/UserList";
 import { auth } from "@/lib/auth";
-import { getUsersWithFollowing } from "@/lib/services/user";
+import {
+	getUsersWithFollowing,
+	type UsersWithFollowing,
+} from "@/lib/services/user";
 
 export default async function FollowingPage({
 	params,
 }: {
 	params: Promise<{ handle: string }>;
 }) {
+	let users: UsersWithFollowing;
+
 	const { handle } = await params;
 	const session = await auth();
-	const users = await getUsersWithFollowing(
-		handle,
-		session ? Number(session.user?.id) : 0,
-	);
+
+	try {
+		users = await getUsersWithFollowing(
+			handle,
+			session ? Number(session.user?.id) : 0,
+		);
+	} catch (err) {
+		console.log(err);
+		return (
+			<div>
+				<h1>유저를 로드 중 오류 발생.</h1>
+			</div>
+		);
+	}
 
 	if (!users.length)
 		return (
