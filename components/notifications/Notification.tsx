c34ecfbf446af1cae302665a9c78e9a6e62d3aa7
Siwoo Jeon <me@migan.co.kr>
2026-01-17 15:18:22 +0900

    fix: hydration

--- components/notifications/Notification.tsx
+++ components/notifications/Notification.tsx
@@ -26,35 +26,42 @@ export default function Notification({
 				className={type === "MENTION" || type === "REPLY" ? "" : "p-2"}
 			>
 				{type === "LIKE" ? (
-					<Link
-						href={`/${post?.author.handle}/posts/${post?.id}`}
-						className="flex gap-x-3.75"
-					>
-						<HeartFill
-							size={25}
-							className="text-pink-500 mt-2 ml-2"
-						/>
-						<div>
-							<Link
-								href={`/${sender.handle}`}
-								className="hover:brightness-80 duration-200"
-							>
-								<UserProfile
-									profile={sender.profile}
-									size={40}
-								/>
-							</Link>
-							<Link href={`/${sender.handle}`}>
+					<div className="relative">
+						<div className="flex gap-x-3.75">
+							<HeartFill
+								size={25}
+								className="text-pink-500 mt-2 ml-2"
+							/>
+							<div>
+								<div className="relative z-10 w-fit h-fit">
+									<Link
+										href={`/${encodeURIComponent(sender.handle)}`}
+										className="hover:brightness-80 duration-200"
+									>
+										<UserProfile
+											profile={sender.profile}
+											size={40}
+										/>
+									</Link>
+								</div>
 								<p>
-									<span className="font-semibold">
-										{sender.name}
+									<span className="relative z-10 font-semibold hover:underline">
+										<Link
+											href={`/${encodeURIComponent(sender.handle)}`}
+										>
+											{sender.name}
+										</Link>
 									</span>
 									님이 당신의 게시물을 좋아합니다.
 								</p>
-							</Link>
-							<p className="text-gray-500">{post?.content}</p>
+								<p className="text-gray-500">{post?.content}</p>
+							</div>
 						</div>
-					</Link>
+						<Link
+							href={`/${encodeURIComponent(post!.author.handle)}/posts/${post?.id}`}
+							className="absolute z-1 w-full h-full top-0 left-0"
+						/>
+					</div>
 				) : null}
 				{type === "FOLLOW" ? (
 					<div className="flex gap-x-2">
@@ -63,18 +70,24 @@ export default function Notification({
 							className="text-blue-500 mt-2 ml-2"
 						/>
 						<div>
-							<Link
-								href={`/${sender.handle}`}
-								className="hover:brightness-80 duration-200"
-							>
-								<UserProfile
-									profile={sender.profile}
-									size={40}
-								/>
-							</Link>
+							<div className="w-fit h-fit">
+								<Link
+									href={`/${sender.handle}`}
+									className="hover:brightness-80 duration-200"
+								>
+									<UserProfile
+										profile={sender.profile}
+										size={40}
+									/>
+								</Link>
+							</div>
 							<p>
-								<span className="font-semibold">
-									{sender.name}
+								<span className="relative z-10 font-semibold hover:underline">
+									<Link
+										href={`/${encodeURIComponent(sender.handle)}`}
+									>
+										{sender.name}
+									</Link>
 								</span>
 								님이 당신을 팔로우 합니다.
 							</p>
@@ -88,32 +101,42 @@ export default function Notification({
 					<Post post={post!} />
 				) : null}
 				{type === "REPOST" ? (
-					<Link
-						href={`/${post?.author.handle}/posts/${post?.id}`}
-						className="flex gap-x-3.75"
-					>
-						<Repeat
-							size={25}
-							className="mt-2 ml-2 text-green-500"
-						/>
-						<div>
-							<Link href={`/${sender.handle}`}>
-								<UserProfile
-									profile={sender.profile}
-									size={40}
-								/>
-							</Link>
-							<Link href={`/${sender.handle}`}>
+					<div className="relative">
+						<div className="flex gap-x-3.75">
+							<Repeat
+								size={25}
+								className="mt-2 ml-2 text-green-500"
+							/>
+							<div>
+								<div className="relative z-10 w-fit h-fit">
+									<Link
+										href={`/${encodeURIComponent(sender.handle)}`}
+										className="hover:brightness-80 duration-200"
+									>
+										<UserProfile
+											profile={sender.profile}
+											size={40}
+										/>
+									</Link>
+								</div>
 								<p>
-									<span className="font-semibold">
-										{sender.name}
+									<span className="relative z-10 font-semibold hover:underline">
+										<Link
+											href={`/${encodeURIComponent(sender.handle)}`}
+										>
+											{sender.name}
+										</Link>
 									</span>
 									님이 당신의 게시물을 재게시 했습니다.
 								</p>
-							</Link>
-							<p className="text-gray-500">{post?.content}</p>
+								<p className="text-gray-500">{post?.content}</p>
+							</div>
 						</div>
-					</Link>
+						<Link
+							href={`/${encodeURIComponent(post!.author.handle)}/posts/${post?.id}`}
+							className="absolute w-full h-full z-1 top-0 left-0"
+						/>
+					</div>
 				) : null}
 			</div>
 		</div>
